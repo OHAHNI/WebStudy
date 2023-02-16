67f6ff8732f51bede45f6f3378281e47b40e600c
sj <tjdwls5657@gmail.com>
2023-02-17 08:51:08 +0900

    seconde commit

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var app = http.createServer(function(request, response){
-    var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    fs.readFile(`descriotions/${queryData.id}`,utf-8,function(data,err){
+        var descriotion = data;
+        
+    })
+    if(_url == '/'){
+        title = 'Welcome';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
+    var template = `<!doctype html>
+    <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href = '/'>WEB</a></h1>
+        <ol>
+            <li><a href = '/?id=html'>HTML</a></li>
+            <li><a href = '/?id=css'>css</a></li>
+            <li><a href = '/?id=javascript'>javascript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>
+            하이퍼 텍스트 마크업 언어(영어: Hyper Text Markup Language, HTML, 문화어: 초본문표식달기언어, 하이퍼본문표식달기언어)는 웹 페이지 표시를 위해 개발된 지배적인 마크업 언어다. 또한, HTML은 제목, 단락, 목록 등과 같은 본문을 위한 구조적 의미를 나타내는 것뿐만 아니라 링크, 인용과 그 밖의 항목으로 구조적 문서를 만들 수 있는 방법을 제공한다. 그안의 꺾쇠 괄호에 둘러싸인 "태그"로 되어있는 HTML 요소 형태로 작성한다. HTML은 웹 브라우저와 같은 HTML 처리 장치의 행동에 영향을 주는 자바스크립트, 본문과 그 밖의 항목의 외관과 배치를 정의하는 CSS 같은 스크립트를 포함하거나 불러올 수 있다. HTML과 CSS 표준의 공동 책임자인 W3C는 명확하고 표상적인 마크업을 위하여 CSS의 사용을 권장한다.[1
+        </p>
+    </body>`
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(template);
 })
 app.listen(3000);
